8ee36744bcee722ac2e4131f61f8c7b878bedd1d
Anant S Awasthy <anant@binarynumbers.io>
2018-04-27 23:51:14 +0530

    Initial Commit

---
+++ SampleListRenderProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleListRenderProject
+//
+//  Created by anantawasthy on 27/04/18.
+//  Copyright © 2018 anantawasthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
